9b7747e82028b44b3b69116c8e8ef71c4722053e
Minh <vokhaiminh0811@gmail.com>
2022-06-08 11:05:40 +0700

    Java fundamental

--- src/myLearning.java
+++ src/myLearning.java
@@ -1,10 +1,61 @@
+package src;
+
+import javax.swing.plaf.synth.SynthSpinnerUI;
+import java.util.Scanner;
+
+
 public class myLearning {
 
-    public static void main(){
-     double myDouble = 122;
+    public static void main(String[] args){
+//     double myFloat = 122.123
+//     System.out.println(myFloat);
+//        boolean myLoop = 1 > 2;
+//        System.out.println(myLoop);
+//
+//        while(myLoop){
+//            System.out.println("Hello World");
+//        }
 
-        System.out.println(myDouble);
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter something dumbass:");
+//        String str = input.nextLine();
+//        System.out.println("You really wrote this? -> " + str);
 
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter some numbers dumbass:");
+//        int n = scanner.nextInt();
+//        while(n != 5){
+//            System.out.println("That is not the correct number dumbass:");
+//            n = scanner.nextInt();
+//        }
+//        System.out.println("That is the correct number");
 
+//        Scanner scanner = new Scanner(System.in);
+//        int n = 0;
+//        do {
+//            System.out.println("Enter some numbers dumbass:");
+//            n = scanner.nextInt();
+//        } while (n != 5);
+//        System.out.println("That is the correct number");
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter a command:");
+//        String str = scanner.nextLine();
+//        switch (str){
+//            case "start":
+//                System.out.println("Machine started!");
+//                break;
+//            case "stop":
+//                System.out.println("Machine stopped");
+//                break;
+//            default:
+//                System.out.println("Wtf wrong command");
+//        }
+        
     }
 }
+
+
+
+
+
