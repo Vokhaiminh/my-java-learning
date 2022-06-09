fee25eae230d8405ffd8d7dbd9feb51e1a21d4d4
Minh <vokhaiminh0811@gmail.com>
2022-06-09 13:47:16 +0700

    Add some new things

--- src/myLearning.java
+++ src/myLearning.java
@@ -52,6 +52,41 @@ public class myLearning {
 //                System.out.println("Wtf wrong command");
 //        }
         
+
+//        int n;
+//        System.out.println("Enter a number:");
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//
+//        for(int i = 1; i <= 10; i++){
+//            n = a * i;
+//            System.out.println("%2d ");
+//        }
+
+
+        // String[] strings = new String[3];
+        // strings[0] = "Dit";
+        // strings[1] = "me";
+        // strings[2] = "may";
+        // System.out.println(strings[1]);
+        // String[] newString = {"Dot", "me", "m"};
+        // System.out.println(newString[1]);
+
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9},
+        };
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){ 
+                System.out.println(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        // System.out.println(arr[1][1]);
+
     }
 }
 
